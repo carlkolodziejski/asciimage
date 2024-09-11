7bae2a89c6dbc6d52a72b23d692caa376e2bfedf
Carl Kolodziejski <carl.kolodziejski19@gmail.com>
2024-09-11 07:21:39 +0200

    Refactorisation complète

--- asciimage/core/converter.py
+++ asciimage/core/converter.py
@@ -7,8 +7,11 @@ from PIL import Image
 
 from InvalidPathException import InvalidPathException
 
+# Constants
+ASCII_CHARS = ["@", "#", "+", "*", ".", " "]
+THRESHOLDS = [50, 100, 150, 200, 230]
 
-# Functions
+
 def select_image():
     root = tk.Tk()
     root.withdraw()
@@ -27,85 +30,69 @@ def select_image():
 
 
 def pixel_to_ascii(pixel_light_value):
-    if pixel_light_value >= 230:
-        return " "
-    elif pixel_light_value >= 200:
-        return "."
-    elif pixel_light_value >= 150:
-        return "*"
-    elif pixel_light_value >= 100:
-        return "+"
-    elif pixel_light_value >= 50:
-        return "#"
-    else:
-        return "@"
+    for threshold, char in zip(THRESHOLDS, ASCII_CHARS):
+        if pixel_light_value < threshold:
+            return char
+    return ASCII_CHARS[-1]
 
 
 def ask_to_save(ascii_result):
     save = input("Do you want to save the ASCII image? (y/n)[default: n] : ")
-    try:
-        if save.lower() == "y":
-            initialdir = os.path.join(os.path.dirname(__file__), '..', '..', 'texts')
-            save_path = filedialog.asksaveasfilename(
-                title="Save the ASCII Image",
-                filetypes=[("Text Files", "*.txt")],
-                initialdir=initialdir
-            )
 
-            if not save_path:
-                raise InvalidPathException("No save path selected")
+    if save.lower() == "y":
+        save_path = select_save_file()
 
-            with open(save_path, "w") as file:
-                file.write(ascii_result)
+        if not save_path:
+            raise InvalidPathException("No save path selected")
 
-            print("ASCII image saved")
-        else:
-            print("ASCII image not saved")
-    except InvalidPathException as e:
-        print(e.message)
-        exit()
+        with open(save_path, "w") as file:
+            file.write(ascii_result)
+            
+        print("ASCII image saved")
+
+    else:
+        print("ASCII image not saved")
 
 
-def show_ascii_image(ascii_array):
-    ascii_result = ""
-    for row in ascii_array:
-        ascii_result = ascii_result + "".join(row) + "\n"
-    ascii_result = ascii_result[:-1]  # Remove the last newline character
+def select_save_file():
+    initialdir = os.path.join(os.path.dirname(__file__), '..', '..', 'texts')
+    save_path = filedialog.asksaveasfilename(
+        title="Save the ASCII Image",
+        filetypes=[("Text Files", "*.txt")],
+        initialdir=initialdir
+    )
+    return save_path
 
+
+def display_ascii_image(ascii_array):
+    ascii_result = "\n".join("".join(row) for row in ascii_array)
     print(ascii_result)
     ask_to_save(ascii_result)
 
 
 def convert_image(image_path):
-    image = Image.open(image_path).convert("RGBA")  # Open in RGBA mode to access alpha channel
-
+    image = Image.open(image_path).convert("RGBA")
     width, height = image.size
-
     image_array = np.array(image)
     ascii_array = np.zeros((height, width), dtype=str)
 
     for i in range(height):
         for j in range(width):
             r, g, b, a = image_array[i, j]
-            if a < 128:  # If alpha value is less than 128, then it is transparent.
+            if a < 128:
                 ascii_array[i, j] = " "
             else:
-                # Convert to grayscale using the luminosity method
                 pixel_light_value = int(0.299 * r + 0.587 * g + 0.114 * b)
                 ascii_array[i, j] = pixel_to_ascii(pixel_light_value)
 
     return ascii_array
 
 
-# Main
-
 if __name__ == "__main__":
     try:
         selected_image = select_image()
+        converted_image = convert_image(selected_image)
+        display_ascii_image(converted_image)
     except InvalidPathException as e:
         print(e.message)
         exit()
-
-    converted_image = convert_image(selected_image)
-
-    show_ascii_image(converted_image)
